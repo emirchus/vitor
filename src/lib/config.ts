c49fe3c3760c51402d6cbb74ae9c50245ca789d2
Emir Ali <emirhassan.ali02@gmail.com>
2024-05-22 12:06:01 -0300

    feat: arreglo url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Vitor",
-  url: process.env.VERCEL_URL || "https://vitor.app",
-  ogImage: `${process.env.VERCEL_URL}/og.png`,
+  url: process.env.NEXT_PUBLIC_VERCEL_URL || "https://vitor.app",
+  ogImage: `${process.env.NEXT_PUBLIC_VERCEL_URL}/og.png`,
   description: "Vitor beautifully Vitor is and accessible and beautiful video editor made with NextJs. Accessible. Customizable. Open Source.",
   links: {
     twitter: "https://twitter.com/emirchus",
